f15a8c8eb9d4797d97a931ba3a444a5e77cd2a3e
DAM Sub 1 <DAM Sub 1@PC05>
2020-11-06 19:01:33 +0100

    escrito hola mundo

--- PantallaPrincipal/src/pantallaprincipal/SegundaClase.java
+++ PantallaPrincipal/src/pantallaprincipal/SegundaClase.java
@@ -9,13 +9,6 @@ package pantallaprincipal;
  *
  * @author DAM Sub 1
  */
-public class PantallaPrincipal {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("Hola Mundo");
-    }
+public class SegundaClase {
     
 }
